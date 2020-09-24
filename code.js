440ba31ce0c54413071b2d102a1f2d88f3ff3e72
Peter Wagner <peter.wagner1986@gmail.com>
2020-09-24 14:11:31 +0200

    a few variables and a function that creats random numbers

---
+++ code.js
@@ -0,0 +1,17 @@
+// Variables
+const enter = document.querySelector(".bars-enter");
+const visualContainer = document.querySelector(".visual-wrapper");
+const visualWidth = visualContainer.clientWidth;
+const visualHeight = visualContainer.clientHeight;
+
+let bars = document.querySelector(".bars-amount");
+
+// Events
+
+
+// Functions
+
+
+function createRandomNumber(max){
+    return Math.floor(Math.random() * (Math.floor(max) + 1));
+};
